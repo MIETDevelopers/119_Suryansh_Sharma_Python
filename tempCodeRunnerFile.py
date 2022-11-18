96d10fa4d31d370477e95b3023950ad9dbeb1bae
Suryansh <2020a1r119@mietjammu.in>
2022-11-18 22:13:02 +0530

    Initial commit

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+    a = float(input('Please Enter the First side of a Triangle: '))
+    b = float(input('Please Enter the Second side of a Triangle: '))
+    c = float(input('Please Enter the Third side of a Triangle: '))
