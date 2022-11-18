20e4d66d4f1308419036b20a6c54d474f0e2f2a6
Suryansh119 <76390896+Suryansh119@users.noreply.github.com>
2022-11-18 22:18:15 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-    a = float(input('Please Enter the First side of a Triangle: '))
-    b = float(input('Please Enter the Second side of a Triangle: '))
-    c = float(input('Please Enter the Third side of a Triangle: '))
